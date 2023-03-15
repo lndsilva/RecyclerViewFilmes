3995d198aeb6690af892a9c888c114bd310f56ed
lndsilva <lndsilva@hotmail.com>
2023-03-14 21:30:17 -0300

    criando o adaptador

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/Filmes.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/Filmes.java
@@ -1,4 +1,51 @@
 package br.com.etecia.recyclerviewfilmes;
 
 public class Filmes {
+    private String titulo;
+    private String descricao;
+    private String categoria;
+    private int imagem;
+
+    //criar o construtor com parâmetros alt+insert
+
+    public Filmes(String titulo, String descricao, String categoria, int imagem) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.imagem = imagem;
+    }
+
+    //criando os métodos de acesso get and setter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
 }
+
---
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -0,0 +1,35 @@
+package br.com.etecia.recyclerviewfilmes;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //criar a classe ViewHolder
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
